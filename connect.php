3be8619cc2fff0cc42c4c002f76078d8658664a8
jobbi <kharsada@gmail.com>
2014-10-03 09:52:58 +0700

    oop cannection

--- connect.php
+++ connect.php
@@ -1,12 +1,24 @@
 <?php 
-
-$connect = pg_connect("host=localhost port=5432 password=15052536 user=postgres dbname=reserve") or die(pg_last_error());
-
+Class Connection{
+	function __construct(){
+	$connect = pg_connect("host=localhost port=5432 password=15052536 user=postgres dbname=reserve") or die(pg_last_error());
+		pg_query("SET NAMES tis620");
 		if(!$connect ){
 		echo "not";
+		exit();
+	}
+		pg_query("SET character_set_client = tis620");
+		pg_query("SET character_set_connection = tis620");
+		pg_query("SET character_set_database = utf8");
+		pg_query("SET character_set_results = tis620");
+		pg_query("SET character_set_server = tis620");
+		pg_query("SET character_set_system = utf8");
+		pg_query("SET collation_connection = tis620_thai_ci");
+		pg_query("SET collation_database = tis620_thai_ci");
+		pg_query("SET collation_server = tis620_thai_ci");
+	
 		}
-		else{
-		echo "เชื่อมต่อผ่าน";
-		}
+}
+
 		  pg_close($connection);
 		?>
